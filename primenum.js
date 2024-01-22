e377c82b3cd46e5cf448c42e0caea929ba57edae
MadgulaVamshi06 <vamshimadgula@gmail.com>
2024-01-22 15:34:07 +0530

    added prime logic

---
+++ primenum.js
@@ -0,0 +1,10 @@
+function isPrime(N) {
+    for (var i = 2; i <= N / 2; i++) {
+        if (N % j === 0) {
+            console.log(N, "is not a prime number");
+            return;
+        }
+    }
+    console.log(N, "is a prime number");
+}
+isPrime(17);
