1ad9c37499084eacc76f3b9a37d69d18da6bd8a0
cssprasanth99 <cssprasanth99@gmail.com>
2024-01-22 15:44:08 +0530

    Corrected variables

--- primenum.js
+++ primenum.js
@@ -1,10 +1,10 @@
 function isPrime(N) {
-    for (var i = 2; i <= N / 2; i++) {
+    for (var j = 2; j <= N / 2; j++) {
         if (N % j === 0) {
-            console.log(N, "is not a prime number");
+            console.log(N, " is not a prime number");
             return;
         }
     }
-    console.log(N, "is a prime number");
+    console.log(N, " is a prime number");
 }
 isPrime(17);
