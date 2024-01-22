b816ce23a55ff60202f660c39715299c1b059fad
MadgulaVamshi06 <vamshimadgula@gmail.com>
2024-01-22 15:41:24 +0530

    code is corrected

--- primenum.js
+++ primenum.js
@@ -1,6 +1,6 @@
 function isPrime(N) {
     for (var i = 2; i <= N / 2; i++) {
-        if (N % j === 0) {
+        if (N % i === 0) {
             console.log(N, "is not a prime number");
             return;
         }
